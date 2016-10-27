c784cb5d75b312bb59222439b701cb2f6453123a
Chris Canal on Macbook Air <christophercanal4@gmail.com>
2016-10-27 17:44:01 -0400

    Added the first try at the most basic SLAM and some print out statements to better understand the data

--- src/monarc_tf_node.cpp
+++ src/monarc_tf_node.cpp
@@ -13,14 +13,72 @@
 
 // pointCloudCallback processes a single PointCloud2 message. We currently have
 // this directly linked to the creation and broadcast of a transform. As we add
-// more data sources through other subscriptions, this will get more complicated. 
+// more data sources through other subscriptions, this will get more complicated.
 // Alternatives to the current approach are  are to:
 // - Buffer the latest value for each subscription and process at a fixed rate
-// - Buffer all but one subscription, and "drive" the broadcast from one
+// - Buffer all but one subscription, and "drive" the broadcast from one (Chris Likes this option)
 // - Use a Time Synchronizer (http://wiki.ros.org/message_filters#Time_Synchronizer)
 //
+
+double avgDistance;
+
+
+//double getMiddleAverage(const sensor_msgs::PointCloud2& cloud, int totalW, int totalH, double percentSize)
+double getMiddleAverage(const sensor_msgs::PointCloud2& cloud)
+{
+	//using percentSize of the field of view at the center
+	//in order to calculate the distance change
+	//from last scan to now. This can be more efficient
+    //computationally once we figure out the best number of
+    //points/size of frame to run this function over.
+    //Assumes the vector of points is streaming in from left
+    //to right and then from top to bottom, so it will
+    //have the entire first row first in order.
+
+	int right = (totalW/2)+(totalW*(percentSize/2));
+	int left = (totalW/2)-(totalW*(percentSize/2));
+	int top = (totalH/2))+(totalH*(percentSize/2);
+    int bottom = (totalH/2)-(totalH*(percentSize/2));
+
+	double count = 0.0;
+	double totalDepth = 0.0;
+
+    sensor_msgs::PointCloud2ConstIterator<float> iter_x(cloud, "x");
+    sensor_msgs::PointCloud2ConstIterator<float> iter_y(cloud, "y");
+    sensor_msgs::PointCloud2ConstIterator<float> iter_z(cloud, "z");
+
+    for (; iter_x != iter_x.end(); ++iter_x, ++iter_y, ++iter_z)
+    {
+        //cout << "X coord = " << (*iter_x) << "; Y coord = " <<  (*iter_y) << "; Z coord = " << (*iter_z) << ";\n";
+        ROS_INFO("monarc_uart_driver using UART port: %a", (*iter_x);
+        ROS_INFO("monarc_uart_driver using UART port: %s", (*iter_x);
+        ROS_INFO("monarc_uart_driver using UART port: %p", (*iter_x);
+        ROS_INFO("monarc_uart_driver using UART port: %i", (*iter_x);
+        ROS_INFO("monarc_uart_driver using UART port: %d", (*iter_x);
+        count += 1.0;
+        totalDepth += (*iter_x);
+        /*
+      // Check if the point is invalid
+      if (!std::isnan (*iter_x) && !std::isnan (*iter_y) && !std::isnan (*iter_z))
+      {
+          if (*iter_y <= right && *iter_y >= left)
+          {
+              if (*iter_z <= top && *iter_z >= bottom)
+              {
+                  cout << "X coord = " << (*iter_x) << "; Y coord = " <<  (*iter_y) << "; Z coord = " << (*iter_z) << ";\n";
+                  count += 1.0;
+                  totalDepth += (*iter_x);
+              }
+          }
+      }*/
+    }
+	return totalDepth/count;
+}
+
 void pointCloudCallback(const sensor_msgs::PointCloud2& msg){
   static tf2_ros::TransformBroadcaster br;
+  avgDistance = getMiddleAverage(msg);
+  //cout << "avgDistance = " << avgDistance << "\n";
 
   geometry_msgs::TransformStamped transformStamped;
   transformStamped.header.stamp = ros::Time::now();
@@ -30,7 +88,7 @@ void pointCloudCallback(const sensor_msgs::PointCloud2& msg){
   transformStamped.transform.translation.x = 0.0;
   transformStamped.transform.translation.y = 0.0;
   transformStamped.transform.translation.z = 0.0;
-  
+
   tf2::Quaternion q;
   q.setRPY(0, 0, 0);
   transformStamped.transform.rotation.x = q.x();
@@ -92,4 +150,3 @@ int main(int argc, char** argv){
   ros::spin();
   return 0;
 };
-
