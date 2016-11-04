32be7d7a870f698f8da5f6159556fd00fb5face2
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2016-11-04 16:09:15 -0400

    Fix frame reference and clean up code

--- src/monarc_tf_node.cpp
+++ src/monarc_tf_node.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <stdexcept>
 
 #include <ros/ros.h>
@@ -8,8 +9,9 @@
 #include <geometry_msgs/TransformStamped.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/point_cloud2_iterator.h>
-#include <iostream>
 
+#include "std_msgs/Float32.h"
+#include "std_msgs/Int32.h"
 #include "monarc_tf/FlyAction.h"
 #include "monarc_tf/FlyGoal.h"
 #include "monarc_uart_driver/FlightControl.h"
@@ -28,6 +30,10 @@ using namespace std;
 double avgDistance = 0.0;
 double pastDistance = 0.0;
 double cloudEdges[6];
+
+ros::Publisher simpleDist;
+ros::Publisher callBackCounter;
+int counter = 1;
 
 void getMinMax(const sensor_msgs::PointCloud2& cloud)
 {
@@ -98,40 +104,40 @@ double getMiddleAverage(const sensor_msgs::PointCloud2& cloud, double totalW, do
 	//using percentSize of the field of view at the center
 	//in order to calculate the distance change
 	//from last scan to now. This can be more efficient
-    //computationally once we figure out the best number of
-    //points/size of frame to run this function over.
-    //Assumes the vector of points is streaming in from left
-    //to right and then from top to bottom, so it will
-    //have the entire first row first in order.
+  //computationally once we figure out the best number of
+  //points/size of frame to run this function over.
+  //Assumes the vector of points is streaming in from left
+  //to right and then from top to bottom, so it will
+  //have the entire first row first in order.
 
 	int right = centerW+(totalW*percentSize/2);
 	int left = centerW-(totalW*percentSize/2);
 	int top = centerH+(totalH*percentSize/2);
-        int bottom = centerH-(totalH*percentSize/2);
+  int bottom = centerH-(totalH*percentSize/2);
 
 	double count = 0.0;
 	double totalDepth = 0.0;
 
-    sensor_msgs::PointCloud2ConstIterator<float> iter_x(cloud, "x");
-    sensor_msgs::PointCloud2ConstIterator<float> iter_y(cloud, "y");
-    sensor_msgs::PointCloud2ConstIterator<float> iter_z(cloud, "z");
+  sensor_msgs::PointCloud2ConstIterator<float> iter_x(cloud, "x");
+  sensor_msgs::PointCloud2ConstIterator<float> iter_y(cloud, "y");
+  sensor_msgs::PointCloud2ConstIterator<float> iter_z(cloud, "z");
 
-    for (; iter_x != iter_x.end(); ++iter_x, ++iter_y, ++iter_z)
+  for (; iter_x != iter_x.end(); ++iter_x, ++iter_y, ++iter_z)
+  {
+    // Check if the point is invalid
+    if (!std::isnan (*iter_x) && !std::isnan (*iter_y) && !std::isnan (*iter_z))
     {
-      // Check if the point is invalid
-      if (!std::isnan (*iter_x) && !std::isnan (*iter_y) && !std::isnan (*iter_z))
+      if (*iter_y <= right && *iter_y >= left)
       {
-          if (*iter_y <= right && *iter_y >= left)
-          {
-              if (*iter_z <= top && *iter_z >= bottom)
-              {
-                 // cout << "X coord = " << (*iter_x) << "; Y coord = " <<  (*iter_y) << "; Z coord = " << (*iter_z) << ";\n";
-                  count += 1.0;
-                  totalDepth += (*iter_x);
-              }
-          }
+        if (*iter_z <= top && *iter_z >= bottom)
+        {
+          // cout << "X coord = " << (*iter_x) << "; Y coord = " <<  (*iter_y) << "; Z coord = " << (*iter_z) << ";\n";
+          count += 1.0;
+          totalDepth += (*iter_x);
+        }
       }
     }
+  }
 	if (count < 1.0)
 	{
 		return 0.0;
@@ -139,7 +145,13 @@ double getMiddleAverage(const sensor_msgs::PointCloud2& cloud, double totalW, do
 	return totalDepth/count;
 }
 
-
+// pointCloudCallback processes a single PointCloud2 message. We currently have
+// this directly linked to the creation and broadcast of a transform. As we add
+// more data sources through other subscriptions, this will get more complicated.
+// Alternatives to the current approach are  are to:
+// - Buffer the latest value for each subscription and process at a fixed rate
+// - Buffer all but one subscription, and "drive" the broadcast from one (Chris Likes this option)
+// - Use a Time Synchronizer (http://wiki.ros.org/message_filters#Time_Synchronizer)
 void pointCloudCallback(const sensor_msgs::PointCloud2& msg){
   static tf2_ros::TransformBroadcaster br;
   getMinMax(msg);
@@ -152,11 +164,11 @@ void pointCloudCallback(const sensor_msgs::PointCloud2& msg){
   cout << "x axis change = " << avgDistance - pastDistance << "\n";
 
   geometry_msgs::TransformStamped transformStamped;
-  transformStamped.header.stamp = ros::Time::now();
-  transformStamped.header.frame_id = "world";
+  transformStamped.header.stamp = msg.header.stamp;
+  transformStamped.header.frame_id = "map";
 
-  transformStamped.child_frame_id = "monarc";
-  transformStamped.transform.translation.x = avgDistance - pastDistance;
+  transformStamped.child_frame_id = msg.header.frame_id;
+  transformStamped.transform.translation.x = avgDistance + pastDistance;
   transformStamped.transform.translation.y = 0.0;
   transformStamped.transform.translation.z = 0.0;
   pastDistance = avgDistance;
@@ -167,8 +179,15 @@ void pointCloudCallback(const sensor_msgs::PointCloud2& msg){
   transformStamped.transform.rotation.y = q.y();
   transformStamped.transform.rotation.z = q.z();
   transformStamped.transform.rotation.w = q.w();
-
+  std_msgs::Float32 nDist;
+  nDist.data = avgDistance;
+  simpleDist.publish(nDist);
+  
+  std_msgs::Int32 callCount;
+  callCount.data = counter;
+  callBackCounter.publish(callCount);
   br.sendTransform(transformStamped);
+  counter++;
 }
 
 typedef actionlib::SimpleActionServer<monarc_tf::FlyAction> Server;
@@ -211,8 +230,9 @@ void executeAction(const monarc_tf::FlyGoalConstPtr& goal, Server* as, ros::Publ
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "monarc_tf_node");
-
   ros::NodeHandle node;
+  simpleDist = node.advertise<std_msgs::Float32>("simpleDist", 0.0);
+  callBackCounter = node.advertise<std_msgs::Int32>("callbackCount", 0);
   ros::Subscriber sub = node.subscribe("/points2", 10, pointCloudCallback);
 
   ros::Publisher flight_command_pub = node.advertise<monarc_uart_driver::FlightControl>("flight_control", 10);
