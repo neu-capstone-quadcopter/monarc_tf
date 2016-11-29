705d927c9192cf8ed66ba1652ba45059a147a23c
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2016-11-28 23:02:02 -0500

    Set up actionlib

--- src/monarc_tf_node.cpp
+++ src/monarc_tf_node.cpp
@@ -1,8 +1,15 @@
+#include <stdexcept>
+
 #include <ros/ros.h>
+#include <actionlib/server/simple_action_server.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <sensor_msgs/PointCloud2.h>
+
+#include "monarc_tf/FlyAction.h"
+#include "monarc_tf/FlyGoal.h"
+#include "monarc_uart_driver/FlightControl.h"
 
 // pointCloudCallback processes a single PointCloud2 message. We currently have
 // this directly linked to the creation and broadcast of a transform. As we add
@@ -34,11 +41,53 @@ void pointCloudCallback(const sensor_msgs::PointCloud2& msg){
   br.sendTransform(transformStamped);
 }
 
+typedef actionlib::SimpleActionServer<monarc_tf::FlyAction> Server;
+
+void executeAction(const monarc_tf::FlyGoalConstPtr& goal, Server* as, ros::Publisher* flight_command_pub) {
+  ros::Rate hover_loop(2);
+  switch (goal->command) {
+    case monarc_tf::FlyGoal::TAKEOFF:
+      // TODO(make decisions based on the command type)
+      std::cout << "TAKEOFF\n";
+      break;
+    case monarc_tf::FlyGoal::LAND:
+      // TODO(make decisions based on the command type)
+      std::cout << "LAND\n";
+      break;
+    case monarc_tf::FlyGoal::HOVER:
+      while (ros::ok() && !as->isNewGoalAvailable()) {
+        std::cout << "HOVER\n";
+        hover_loop.sleep();
+      }
+      ROS_INFO("HOVER CANCELLED");
+      return;
+    default:
+      throw std::invalid_argument("unhandled command type");
+  }
+
+  int wait = 0;
+  ros::Rate loop_rate(100);
+  while (ros::ok()) {
+    // TODO read shit from data sources and make decisions. Publish on flight_command_pub.
+    loop_rate.sleep();
+    if (wait > 200) {
+      break;
+    }
+    wait++;
+  }
+
+  as->setSucceeded();
+}
+
 int main(int argc, char** argv){
   ros::init(argc, argv, "monarc_tf_node");
 
   ros::NodeHandle node;
   ros::Subscriber sub = node.subscribe("/points2", 10, pointCloudCallback);
+
+  ros::Publisher flight_command_pub = node.advertise<monarc_uart_driver::FlightControl>("flight_control", 10);
+  Server server(node, "fly", boost::bind(&executeAction, _1, &server, &flight_command_pub), false);
+  server.start();
 
   ros::spin();
   return 0;
